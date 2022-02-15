4b200cfa65fd1189f6ad88aaef275d676238461e
Jennifer03333333332 <yingxual@andrew.cmu.edu>
2022-02-15 18:34:17 -0500

    test

--- Assignments/DrawSVG/src/texture.cpp
+++ Assignments/DrawSVG/src/texture.cpp
@@ -104,43 +104,63 @@ inline Color lerpColor(T ratio, Color start, Color ends){
   //return start + ratio * ( ends + (-1)*start);
     return (1 - ratio) * start + ratio * ends;
 }
-
+//x,y are texture's coordinates
 inline Color GetColorFromTexure(Texture& tex, const int& level, const int& x, const int& y){
-  float r = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width)]/ 255.0;
-  float g = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width) + 1]/ 255.0;
-  float b = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width) + 2]/ 255.0;
-  float a = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width) + 3]/ 255.0;
+  float r = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width)]/ 255.0f
+  float g = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width) + 1]/ 255.0f;
+  float b = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width) + 2]/ 255.0f;
+  float a = tex.mipmap[level].texels[4 * (x + y * tex.mipmap[level].width) + 3]/ 255.0f;
   return Color(r,g,b,a);
 }
 
-Color Sampler2DImp::sample_bilinear(Texture& tex,
-    float u, float v,
-    int level) {
-    if (level >= tex.mipmap.size()) {
-        return Color(1, 0, 1, 1);
+//u,v are [0,1] coordinates
+Color Sampler2DImp::sample_bilinear(Texture& tex, float u, float v, int level) {
+    if (level < 0 || level >= tex.mipmap.size()) return Color(1, 0, 1, 1);
+    float su = clamp(u, 0.0f, 0.99f) * tex.mipmap[level].width;//clamp the edges
+    float sv = clamp(v, 0.0f, 0.99f) * tex.mipmap[level].height;
+    //texel's center
+    float u0 = floor(su) + 0.5f; float v0 = floor(sv) + 0.5f;
+    float u1, v1;
+    if (su - (int)su < 0.5f) {//
+        u1 = clamp<float>(u0-1, 0.0, tex.mipmap[level].width);//if out of range : constrain
+        swap(u1, u0);
     }
-
-    MipLevel& mip = tex.mipmap[level];
-    u = u * mip.width - 0.5f;
-    v = v * mip.height - 0.5f;
-    size_t x = floor(u);
-    size_t y = floor(v);
-
-    float u_ratio = u - x;
-    float v_ratio = v - y;
-    float u_opposite = 1 - u_ratio;
-    float v_opposite = 1 - v_ratio;
-
-    Color c1, c2, c3, c4;
-    uint8_to_float(&c1.r, &mip.texels[4 * (x + mip.width * y)]);
-    uint8_to_float(&c2.r, &mip.texels[4 * (x + 1 + mip.width * y)]);
-    uint8_to_float(&c3.r, &mip.texels[4 * (x + mip.width * (y + 1))]);
-    uint8_to_float(&c4.r, &mip.texels[4 * (x + 1 + mip.width * (y + 1))]);
-
-    Color f = (c1 * u_opposite + c2 * u_ratio) * v_opposite +
-        (c3 * u_opposite + c4 * u_ratio) * v_ratio;
-    return f;
+    else { u1 = clamp<float>(u0+1, 0.0, tex.mipmap[level].width); }
+    if (sv - (int)sv < 0.5f) {
+        v1 = clamp<float>(v0-1, 0.0, tex.mipmap[level].height);
+        swap(v1, v0);
+    }
+    else { v1 = clamp<float>(v0+1, 0, tex.mipmap[level].height); }
+    Color lerpHorizontal1 = lerpColor((su - u0) / (u1 - u0), GetColorFromTexure(tex, level, u0, v0), GetColorFromTexure(tex, level, u1, v0));
+    Color lerpHorizontal2 = lerpColor((su - u0) / (u1 - u0), GetColorFromTexure(tex, level, u0, v1), GetColorFromTexure(tex, level, u1, v1));
+    Color lerpVertical = lerpColor((sv - v0) / (v1 - v0), lerpHorizontal1, lerpHorizontal2);
+    return lerpVertical;
 }
+
+//Color Sampler2DImp::sample_bilinear(Texture& tex, float u, float v, int level) {
+//    if (level < 0 || level >= tex.mipmap.size()) {
+//        return Color(1, 0, 1, 1);
+//    }
+//
+//    MipLevel& mip = tex.mipmap[level];
+//    u = u * mip.width - 0.5f;v = v * mip.height - 0.5f;
+//    size_t x = floor(u);size_t y = floor(v);
+//
+//    float u_ratio = u - x;
+//    float v_ratio = v - y;
+//    float u_opposite = 1 - u_ratio;
+//    float v_opposite = 1 - v_ratio;
+//
+//    Color c1, c2, c3, c4;
+//    uint8_to_float(&c1.r, &mip.texels[4 * (x + mip.width * y)]);
+//    uint8_to_float(&c2.r, &mip.texels[4 * (x + 1 + mip.width * y)]);
+//    uint8_to_float(&c3.r, &mip.texels[4 * (x + mip.width * (y + 1))]);
+//    uint8_to_float(&c4.r, &mip.texels[4 * (x + 1 + mip.width * (y + 1))]);
+//
+//    Color f = (c1 * u_opposite + c2 * u_ratio) * v_opposite +
+//        (c3 * u_opposite + c4 * u_ratio) * v_ratio;
+//    return f;
+//}
 
 Color Sampler2DImp::sample_trilinear(Texture& tex, 
                                      float u, float v, 
