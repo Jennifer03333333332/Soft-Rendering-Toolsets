bb3c5a470ffcb1dc0c542610448a9082e80cc584
Jennifer03333333332 <yingxual@andrew.cmu.edu>
2022-03-28 01:08:00 -0400

    A3 Task2 Ray-Sphere(Haven't check)

--- Assignments/Scotty3D/src/student/shapes.cpp
+++ Assignments/Scotty3D/src/student/shapes.cpp
@@ -18,7 +18,50 @@ Trace Sphere::hit(const Ray& ray) const {
 
     // TODO (PathTracer): Task 2
     // Intersect this ray with a sphere of radius Sphere::radius centered at the origin.
+    bool hit = true;
+    float t = 0;
+    //Quadratic equation
+    float a = (ray.dir).norm_squared();
+    float b = 2.0f*dot(ray.point,ray.dir);
+    float c = (ray.point).norm_squared() - radius*radius;
 
+    float delta = b*b - 4.0f*a*c;//b^2 - 4ac
+    //2 intersection points
+    if(delta > 0){
+        bool t1_valid = true, t2_valid = true;
+        float t1 = ((-2.0f)*dot(ray.point,ray.dir) + sqrt(delta))/(2.0f*(ray.dir).norm_squared());
+        float t2 = ((-2.0f)*dot(ray.point,ray.dir) - sqrt(delta))/(2.0f*(ray.dir).norm_squared());
+        //check t > 0
+        t1_valid = (t1<0)? false:true;
+        t2_valid = (t2<0)? false:true;
+        //check bounds
+        //4 Cases: t1 out of bounds but t2 in the bounds; t1 in the bounds but t2 outof bounds
+        float distance1 = std::abs((t1 * ray.dir).norm());
+        float distance2 = std::abs((t2 * ray.dir).norm());
+        if(distance1 < ray.dist_bounds.x || distance1 > ray.dist_bounds.y) {
+            t1_valid = false;
+        }
+        if(distance2 < ray.dist_bounds.x || distance2 > ray.dist_bounds.y) {
+            t2_valid = false;
+        }
+        // Conclusion
+        if( t1_valid && t2_valid ){
+            t = std::min(t1,t2);
+        }
+        else if((!t1_valid) && (!t2_valid)){
+            hit = false;
+        }
+        else{
+            t = (t1_valid)? t1:t2;
+        }
+
+    }
+    else if (delta == 0){//1 intersection point: tangent
+        t = ((-2.0f)*dot(ray.point,ray.dir))/(2.0f*(ray.dir).norm_squared());
+    }
+    else{// No intersection
+        hit = false;
+    }
     // If the ray intersects the sphere twice, ret should
     // represent the first intersection, but remember to respect
     // ray.dist_bounds! For example, if there are two intersections,
@@ -27,10 +70,12 @@ Trace Sphere::hit(const Ray& ray) const {
 
     Trace ret;
     ret.origin = ray.point;
-    ret.hit = false;       // was there an intersection?
-    ret.distance = 0.0f;   // at what distance did the intersection occur?
-    ret.position = Vec3{}; // where was the intersection?
-    ret.normal = Vec3{};   // what was the surface normal at the intersection?
+    ret.hit = hit;       // was there an intersection?
+    if(hit){
+        ret.distance = std::abs((ray.at(t) - ray.point).norm());   // at what distance did the intersection occur?
+        ret.position = ray.at(t); // where was the intersection?
+        ret.normal = ray.at(t) - Vec3();   // TODO normals should be out-facing.
+    }
     return ret;
 }
 
